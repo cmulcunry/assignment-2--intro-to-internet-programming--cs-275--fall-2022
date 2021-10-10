195eb36c4c5be209438ab8825db5c80d705ac2cb
rrliang <42505429+rrliang@users.noreply.github.com>
2021-10-10 13:06:42 -0400

    Put back gulpfile
    Accidentally pushed js in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,96 +1,184 @@
-let whiteRice = document.getElementById(`whiteRice`);
-let caliRice = document.getElementById(`caliRice`);
-let recipe = document.getElementById(`recipe`);
-var cups = document.getElementById(`cups`).value;
-var currentRice = `white`;
+const { src, dest, series, watch } = require(`gulp`);
+const gulp = require('gulp');
+const del = require(`del`);
+const babel = require(`gulp-babel`);
+const htmlCompressor = require(`gulp-htmlmin`);
+const htmlValidator = require(`gulp-html`);
+const cssLinter = require(`gulp-stylelint`);
+const jsLinter = require(`gulp-eslint`);
+const jsCompressor = require(`gulp-uglify`);
+const imageCompressor = require(`gulp-imagemin`);
+const cache = require(`gulp-cache`);
+const browserSync = require(`browser-sync`);
+const reload = browserSync.reload;
+let browserChoice = `default`;
 
-/*https://stackoverflow.com/questions/511088/use-javascript-to-place-cursor-
-at-end-of-text-in-text-input-element?page=2&tab=Oldest*/
-window.onload = function() {
-    const end = document.getElementById(`cups`).value.length;
-    document.getElementById(`cups`).setSelectionRange(end, end);
-    document.getElementById(`cups`).focus();
-    document.getElementById(`whiteRice`).click();
-};
-
-/*https://stackoverflow.com/questions/175739/built-in-way-in-javascript-to-
-check-if-a-string-is-a-valid-number?page=1&tab=votes#tab-top*/
-/*https://stackoverflow.com/questions/45827/how-do-you-automatically-set-the-
-focus-to-a-textbox-when-a-web-page-loads*/
-function isNumeric(str) {
-    if (typeof str != `string`) return false; // we only process strings!
-    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string
-    !isNaN(parseFloat(str)) &&
-    str > 0; // ...and ensure strings of whitespace fail
+async function safari () {
+    browserChoice = `safari`;
 }
 
-/*https://stackoverflow.com/questions/45399923/eslint-disable-warning-defined-
-but-never-used-for-specific-function/51295375*/
-function listener() { // eslint-disable-line no-unused-vars
-    cups = document.getElementById(`cups`).value;
-    if (!isNumeric(cups)) {
-        recipe.textContent =
-        `Error, please input a number.`;
-    } else {
-        if (currentRice == `white`) {
-            recipe.textContent =
-            `Combine `+ cups +` cup(s) of rice with ` +
-            Math.round((cups * 2)*100)/100 +
-            ` cups of water and 1 Tbsp olive oil.\n` +
-            `Bring to a boil, then reduce heat to the lowest setting.\n` +
-            `Cook for about 18 minutes.`;
-        } else if (currentRice == `cali`) {
-            recipe.textContent =
-            `For slightly al dente rice: Combine ` + cups +
-            ` cup(s) of rice with  ` +
-            Math.round((cups * 1.6)*100)/100 + ` cups of water or broth and 1 ` +
-            `Tbsp olive oil.\n Bring to a boil and stir once to mix.\n` +
-            `Reduce heat to low, cover with a tight-fitting lid and cook for 25 ` +
-            `minutes.\n` +
-            `Remove from heat and let stand for 5 minutes.\n` +
-            `Fluff with a fork and serve.\n` +
-            `For softer rice: Increase liquid by 1/2 cup and cook time by ` +
-            `5 minutes.`;
+async function firefox () {
+    browserChoice = `firefox`;
+}
+
+async function chrome () {
+    browserChoice = `google chrome`;
+}
+
+async function opera () {
+    browserChoice = `opera`;
+}
+
+async function edge () {
+    browserChoice = `microsoft-edge`;
+}
+
+async function allBrowsers () {
+    browserChoice = [
+        `safari`,
+        `firefox`,
+        `google chrome`,
+        `opera`,
+        `microsoft-edge`
+    ];
+}
+
+let validateHTML = () => {
+    return gulp.src(
+		`html/*.html`)
+        .pipe(htmlValidator());
+};
+
+let compressHTML = () => {
+    return src([`html/*.html`,`html/**/*.html`])
+        .pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(dest(`prod`));
+};
+
+let transpileJSForDev = () => {
+    return src(`js/*.js`)
+        .pipe(babel())
+        .pipe(dest(`temp/js`));
+};
+
+let transpileJSForProd = () => {
+    return src(`js/*.js`)
+        .pipe(babel())
+        .pipe(jsCompressor())
+        .pipe(dest(`prod/js`));
+};
+
+let lintCSS = () => {
+	return src(`css/*.css`)
+		.pipe(cssLinter());
+}
+
+let lintJS = () => {
+    return src(`js/*.js`)
+		.pipe(jsLinter())
+        .pipe(jsLinter.format());
+};
+
+let copyUnprocessedAssetsForProd = () => {
+    return src([
+        `*.*`,       // Source all files,
+        `**`,        // and all folders,
+        `!html/`,    // but not the HTML folder
+        `!html/*.*`, // or any files in it
+        `!html/**`,  // or any sub folders;
+        `!**/*.js`,  // ignore JS;
+        `!css/**` // and, ignore Sass/CSS.
+    ], {dot: true}).pipe(dest(`prod`));
+};
+
+let serve = () => {
+    browserSync({
+        notify: true,
+        port: 9000,
+        reloadDelay: 50,
+        browser: browserChoice,
+        server: {
+            baseDir: [
+                `temp`,
+                `css`,
+                `html`,
+				`js`,
+            ]
+        }
+    });
+
+    watch(`js/*.js`,
+        series(lintJS, transpileJSForDev)
+    ).on(`change`, reload);
+
+    watch(`css/*.css`,
+        series(lintCSS)
+    ).on(`change`, reload);
+
+    watch(`html/*.html`,
+        series(validateHTML)
+    ).on(`change`, reload);
+};
+
+async function clean() {
+    let fs = require(`fs`),
+        i,
+        foldersToDelete = [`temp`, `prod`];
+
+    for (i = 0; i < foldersToDelete.length; i++) {
+        try {
+            fs.accessSync(foldersToDelete[i], fs.F_OK);
+            process.stdout.write(`\n\tThe ` + foldersToDelete[i] +
+                ` directory was found and will be deleted.\n`);
+            del(foldersToDelete[i]);
+        } catch (e) {
+            process.stdout.write(`\n\tThe ` + foldersToDelete[i] +
+                ` directory does NOT exist or is NOT accessible.\n`);
         }
     }
+
+    process.stdout.write(`\n`);
 }
 
-/*https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-
-decimal-places-if-necessary*/
-let whiteRiceRecipe =
-    `Combine `+ cups +` cup(s) of rice with ` + Math.round((cups * 2)*100)/100 +
-    ` cups of water and 1 Tbsp olive oil.\n` +
-    `Bring to a boil, then reduce heat to the lowest setting.\n` +
-    `Cook for about 18 minutes.`;
+async function listTasks () {
+    let exec = require(`child_process`).exec;
 
-let caliRiceRecipe =
-    `For slightly al dente rice: Combine ` + cups +
-    ` cup(s) of rice with  ` +
-    Math.round((cups * 1.6)*100)/100 + ` cups of water or broth and 1 Tbsp ` +
-    `olive oil.\n` +
-    `Bring to a boil and stir once to mix.\n` +
-    `Reduce heat to low, cover with a tight-fitting lid and cook for 25 ` +
-    `minutes.\n` +
-    `Remove from heat and let stand for 5 minutes.\n` +
-    `Fluff with a fork and serve.\n` +
-    `For softer rice: Increase liquid by 1/2 cup and cook time by 5 minutes.`;
+    exec(`gulp --tasks`, function (error, stdout, stderr) {
+        if (null !== error) {
+            process.stdout.write(`An error was likely generated when invoking ` +
+                `the “exec” program in the default task.`);
+        }
 
-whiteRice.onclick = () => {
-    currentRice = `white`;
-    recipe.textContent = whiteRiceRecipe;
-    whiteRice.style.background=`#bd8f97`;
-    whiteRice.style.color=`white`;
-    whiteRice.style.border=`1px solid white`;
-    caliRice.style.border=`none`;
-    caliRice.style.background=`grey`;
-};
+        if (`` !== stderr) {
+            process.stdout.write(`Content has been written to the stderr stream ` +
+                `when invoking the “exec” program in the default task.`);
+        }
 
-caliRice.onclick = () => {
-    currentRice = `cali`;
-    recipe.textContent = caliRiceRecipe;
-    caliRice.style.background=`#bd8f97`;
-    caliRice.style.color=`white`;
-    caliRice.style.border=`1px solid white`;
-    whiteRice.style.border=`none`;
-    whiteRice.style.background=`grey`;
-};
+        process.stdout.write(`\n\tThis default task does ` +
+            `nothing but generate this message. The ` +
+            `available tasks are:\n\n${stdout}`);
+    });
+}
+
+exports.safari = series(safari, serve);
+exports.firefox = series(firefox, serve);
+exports.chrome = series(chrome, serve);
+exports.opera = series(opera, serve);
+exports.edge = series(edge, serve);
+exports.safari = series(safari, serve);
+exports.allBrowsers = series(allBrowsers, serve);
+exports.validateHTML = validateHTML;
+exports.compressHTML = compressHTML;
+exports.transpileJSForDev = transpileJSForDev;
+exports.transpileJSForProd = transpileJSForProd;
+exports.lintJS = lintJS;
+exports.lintCSS = lintCSS;
+exports.copyUnprocessedAssetsForProd = copyUnprocessedAssetsForProd;
+exports.build = series(
+	compressHTML,
+    transpileJSForProd,
+    copyUnprocessedAssetsForProd
+);
+exports.dev = series(lintJS, lintCSS, transpileJSForDev, validateHTML, serve);
+exports.clean = clean;
+exports.default = listTasks;
