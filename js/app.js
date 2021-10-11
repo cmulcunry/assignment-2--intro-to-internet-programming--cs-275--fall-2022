b63280d0fa08838dc077e498c8b4d3c0e5c20a24
rrliang <42505429+rrliang@users.noreply.github.com>
2021-10-10 22:35:51 -0400

    Updated js file
    -removed unnecessary code 
    -fixed it so changing rice options would keep cup number from text box

--- js/app.js
+++ js/app.js
@@ -13,6 +13,29 @@ window.onload = function() {
     document.getElementById(`whiteRice`).click();
 };
 
+/*https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-
+decimal-places-if-necessary*/
+function setWhiteRice(cups) {
+    return `Combine `+ cups +` cup(s) of rice with ` +
+        Math.round((cups * 2)*100)/100 +
+        ` cups of water and 1 Tbsp olive oil.\n` +
+        `Bring to a boil, then reduce heat to the lowest setting.\n` +
+        `Cook for about 18 minutes.`;
+}
+
+function setCaliRice(cups) {
+    return `For slightly al dente rice: Combine ` + cups +
+        ` cup(s) of rice with  ` +
+        Math.round((cups * 1.6)*100)/100 + ` cups of water or broth and 1 Tbsp ` +
+        `olive oil.\n` +
+        `Bring to a boil and stir once to mix.\n` +
+        `Reduce heat to low, cover with a tight-fitting lid and cook for 25 ` +
+        `minutes.\n` +
+        `Remove from heat and let stand for 5 minutes.\n` +
+        `Fluff with a fork and serve.\n` +
+        `For softer rice: Increase liquid by 1/2 cup and cook time by 5 minutes.`;
+}
+
 /*https://stackoverflow.com/questions/175739/built-in-way-in-javascript-to-
 check-if-a-string-is-a-valid-number?page=1&tab=votes#tab-top*/
 /*https://stackoverflow.com/questions/45827/how-do-you-automatically-set-the-
@@ -33,51 +56,18 @@ function listener() { // eslint-disable-line no-unused-vars
         `Error, please input a number.`;
     } else {
         if (currentRice == `white`) {
-            recipe.textContent =
-            `Combine `+ cups +` cup(s) of rice with ` +
-            Math.round((cups * 2)*100)/100 +
-            ` cups of water and 1 Tbsp olive oil.\n` +
-            `Bring to a boil, then reduce heat to the lowest setting.\n` +
-            `Cook for about 18 minutes.`;
+            recipe.textContent = recipe.textContent =
+            setWhiteRice(document.getElementById(`cups`).value);
         } else if (currentRice == `cali`) {
-            recipe.textContent =
-            `For slightly al dente rice: Combine ` + cups +
-            ` cup(s) of rice with  ` +
-            Math.round((cups * 1.6)*100)/100 + ` cups of water or broth and 1 ` +
-            `Tbsp olive oil.\n Bring to a boil and stir once to mix.\n` +
-            `Reduce heat to low, cover with a tight-fitting lid and cook for 25 ` +
-            `minutes.\n` +
-            `Remove from heat and let stand for 5 minutes.\n` +
-            `Fluff with a fork and serve.\n` +
-            `For softer rice: Increase liquid by 1/2 cup and cook time by ` +
-            `5 minutes.`;
+            recipe.textContent = recipe.textContent =
+            setCaliRice(document.getElementById(`cups`).value);
         }
     }
 }
 
-/*https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-
-decimal-places-if-necessary*/
-let whiteRiceRecipe =
-    `Combine `+ cups +` cup(s) of rice with ` + Math.round((cups * 2)*100)/100 +
-    ` cups of water and 1 Tbsp olive oil.\n` +
-    `Bring to a boil, then reduce heat to the lowest setting.\n` +
-    `Cook for about 18 minutes.`;
-
-let caliRiceRecipe =
-    `For slightly al dente rice: Combine ` + cups +
-    ` cup(s) of rice with  ` +
-    Math.round((cups * 1.6)*100)/100 + ` cups of water or broth and 1 Tbsp ` +
-    `olive oil.\n` +
-    `Bring to a boil and stir once to mix.\n` +
-    `Reduce heat to low, cover with a tight-fitting lid and cook for 25 ` +
-    `minutes.\n` +
-    `Remove from heat and let stand for 5 minutes.\n` +
-    `Fluff with a fork and serve.\n` +
-    `For softer rice: Increase liquid by 1/2 cup and cook time by 5 minutes.`;
-
 whiteRice.onclick = () => {
     currentRice = `white`;
-    recipe.textContent = whiteRiceRecipe;
+    listener();
     whiteRice.style.background=`#bd8f97`;
     whiteRice.style.color=`white`;
     whiteRice.style.border=`1px solid white`;
@@ -87,7 +77,7 @@ whiteRice.onclick = () => {
 
 caliRice.onclick = () => {
     currentRice = `cali`;
-    recipe.textContent = caliRiceRecipe;
+    listener();
     caliRice.style.background=`#bd8f97`;
     caliRice.style.color=`white`;
     caliRice.style.border=`1px solid white`;
