9279700c7df12ad6becca09ab9243edd2123e3b3
woodsweber <woodswebe@hartford.edu>
2021-10-13 09:19:25 -0400

    Replaced two buttons with one button that toggles between two views

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 /*const WHITE = document.getElementById(`white`);
 const CALIFORNIA = document.getElementById(`california`);
+const INPUT = document.getElementById(`input`)
 
 WHITE.style.display = `none`;
 WHITE.style.display = `block`;
@@ -10,7 +11,7 @@ CALIFORNIA.style.display = `block`;*/
 const WHITE = document.getElementById(`white`);
 const CALIFORNIA = document.getElementById(`california`);
 
-let toggleCalifornia = () => {
+/*let toggleCalifornia = () => {
     if(CALIFORNIA.style.display === `none`) CALIFORNIA.style.display = `block`;
     else CALIFORNIA.style.display = `none`;
 };
@@ -18,4 +19,15 @@ let toggleCalifornia = () => {
 let toggleWhite = () => {
     if(WHITE.style.display === `none`) WHITE.style.display = `block`;
     else WHITE.style.display = `none`;
+};*/
+
+let toggleButton = () => {
+    if(CALIFORNIA.style.display === `none`) {
+        CALIFORNIA.style.display = `block`;
+        WHITE.style.display = `none`;
+    }
+    else {
+        CALIFORNIA.style.display = `none`;
+        WHITE.style.display = `block`;
+    }
 };
