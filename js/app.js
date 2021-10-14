e8774b2abc3679a4326118ff2d0e9caf428f5f04
woodsweber <woodswebe@hartford.edu>
2021-10-14 00:31:24 -0400

    More work on decimal to fraction conversion

--- js/app.js
+++ js/app.js
@@ -19,15 +19,15 @@ let toggleRecipe = () => {
 
 let updateQuantities = () => {
     let x = QUANTITY.value;
-    CALIFORNIA.children[2].innerHTML = `1. Combine ${1*x} cups of rice with ${1.6*x} cups of water or broth and ${.8*x} Tbsp olive oil.`;
-    CALIFORNIA.children[6].innerHTML = `For softer rice, increase liquid by ${.4*x} cup and cook time by 5 minutes.`;
-    WHITE.children[1].innerHTML = `1. Combine ${1*x} cup of rice with ${2*x} cups of water and ${1*x} Tbsp olive oil.`;
+    CALIFORNIA.children[2].innerHTML = `1. Combine ${decToFrac(1*x)} cups of rice with ${decToFrac(1.6*x)} cups of water or broth and ${decToFrac(.8*x)} Tbsp olive oil.`;
+    CALIFORNIA.children[6].innerHTML = `For softer rice, increase liquid by ${decToFrac(.4*x)} cup and cook time by 5 minutes.`;
+    WHITE.children[1].innerHTML = `1. Combine ${decToFrac(1*x)} cup of rice with ${decToFrac(2*x)} cups of water and ${decToFrac(1*x)} Tbsp olive oil.`;
 };
 
 //SELECT.addEventListener(`change`, toggleRecipe());
 
-/*let gcd = (n,d) => {
-    if(d==0) return n;
+let gcd = (n,d) => {
+    if(d===0) return n;
     return gcd(d, n%d | 0);
 };
 
@@ -39,7 +39,10 @@ let decToFrac = (x) => {
     let y = x | 0;
     let denominator = 10**length(x-y);
     let numerator = (x-y)*denominator;
-    denominator = denominator/gcd(numerator, denominator);
-    numerator = (numerator/gcd(numerator, denominator))/2;
-    return `${y} ${numerator | 0}/${denominator}`;
-};*/
+    let g = gcd(numerator, denominator);
+    denominator = denominator/g;
+    numerator = numerator/g;
+    if(numerator !== 0 && y !== 0) return `${y} ${parseFloat(numerator).toPrecision(length(x-y))}/${denominator}`;
+    else if(numerator !== 0) return `${parseFloat(numerator).toPrecision(length(x-y))}/${denominator}`;
+    else return `${y}`;
+};
