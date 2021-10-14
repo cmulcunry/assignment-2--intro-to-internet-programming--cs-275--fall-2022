5a2bb271aa30803aafe21773a268d918d79e69f3
BorjaErika <78446815+BorjaErika@users.noreply.github.com>
2021-10-14 14:53:07 -0400

    updating gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -162,22 +162,6 @@ let serve = () => {
 
 };
 
-async function clean() {
-    let fs = require(`fs`),
-        i,
-        foldersToDelete = [`./temp`, `prod`];
-
-    for (i = 0; i < foldersToDelete.length; i++) {
-        try {
-            fs.accessSync(foldersToDelete[i], fs.F_OK);
-            process.stdout.write(`\n\tThe ` + foldersToDelete[i] +
-                ` directory was found and will be deleted.\n`);
-            del(foldersToDelete[i]);
-        } catch (e) {
-            process.stdout.write(`\n\tThe ` + foldersToDelete[i] +
-                ` directory does NOT exist or is NOT accessible.\n`);
-        }
-    }
 
     process.stdout.write(`\n`);
 }
