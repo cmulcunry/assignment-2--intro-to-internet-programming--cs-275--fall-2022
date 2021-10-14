15c4cd14e2d323517876444cb79ff4909d1d25b2
woodsweber <woodswebe@hartford.edu>
2021-10-13 23:46:16 -0400

    Fixed some code formatting issues

--- js/app.js
+++ js/app.js
@@ -4,8 +4,6 @@ const BUTTON = document.getElementById(`button`);
 const QUANTITY = document.getElementById(`input`);
 //const SELECT = document.getElementById(`select`);
 
-
-
 let toggleRecipe = () => {
     if(CALIFORNIA.style.display === `none`) {
         CALIFORNIA.style.display = `block`;
@@ -21,14 +19,14 @@ let toggleRecipe = () => {
 
 let updateQuantities = () => {
     let x = QUANTITY.value;
-    CALIFORNIA.children[2].innerHTML = `1. Combine ${1*x} cups of rice with ${decToFrac(1.6*x)} cups of water or broth and ${.8*x} Tbsp olive oil.`;
+    CALIFORNIA.children[2].innerHTML = `1. Combine ${1*x} cups of rice with ${1.6*x} cups of water or broth and ${.8*x} Tbsp olive oil.`;
     CALIFORNIA.children[6].innerHTML = `For softer rice, increase liquid by ${.4*x} cup and cook time by 5 minutes.`;
     WHITE.children[1].innerHTML = `1. Combine ${1*x} cup of rice with ${2*x} cups of water and ${1*x} Tbsp olive oil.`;
 };
 
 //SELECT.addEventListener(`change`, toggleRecipe());
 
-let gcd = (n,d) => {
+/*let gcd = (n,d) => {
     if(d==0) return n;
     return gcd(d, n%d | 0);
 };
@@ -44,4 +42,4 @@ let decToFrac = (x) => {
     denominator = denominator/gcd(numerator, denominator);
     numerator = (numerator/gcd(numerator, denominator))/2;
     return `${y} ${numerator | 0}/${denominator}`;
-};
+};*/
