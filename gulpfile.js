2cd53286fc4771302ba0b6fa111ffc22329382b6
BorjaErika <78446815+BorjaErika@users.noreply.github.com>
2021-10-14 11:12:50 -0400

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,16 @@ const { src, dest, series } = require(`gulp`);
 const htmlValidator = require(`gulp-html`);
 const htmlCompressor = require(`gulp-htmlmin`);
 
-let validateHTML = () => {
+   let validateHTML = () => {
     return src([`html/*.html`, `html/**/*.html`]).pipe(htmlValidator());
-};
+   };
 
-let compressHTML = () => {
+  let compressHTML = () => {
     return src([`html/*.html`,`html/**/*.html`])
         .pipe(htmlCompressor({collapseWhitespace: true}))
         .pipe(dest(`prod`));
-};
+    };
 
-exports.validateHTML = validateHTML;
-exports.compressHTML = compressHTML;
-exports.HTMLProcessing = series(validateHTML, compressHTML);
+   exports.validateHTML = validateHTML;
+   exports.compressHTML = compressHTML;
+   exports.HTMLProcessing = series(validateHTML, compressHTML);
