3dd420e7bd6c73551c801d8c0a13515eeba35845
woodsweber <woodswebe@hartford.edu>
2021-10-13 22:23:20 -0400

    Deleted unused code and updated function name to reflect function usage

--- js/app.js
+++ js/app.js
@@ -1,27 +1,8 @@
-/*const WHITE = document.getElementById(`white`);
-const CALIFORNIA = document.getElementById(`california`);
-const INPUT = document.getElementById(`input`)
-
-WHITE.style.display = `none`;
-WHITE.style.display = `block`;
-
-CALIFORNIA.style.display = `none`;
-CALIFORNIA.style.display = `block`;*/
-
 const WHITE = document.getElementById(`white`);
 const CALIFORNIA = document.getElementById(`california`);
 const BUTTON = document.getElementById(`button`);
-/*let toggleCalifornia = () => {
-    if(CALIFORNIA.style.display === `none`) CALIFORNIA.style.display = `block`;
-    else CALIFORNIA.style.display = `none`;
-};
 
-let toggleWhite = () => {
-    if(WHITE.style.display === `none`) WHITE.style.display = `block`;
-    else WHITE.style.display = `none`;
-};*/
-
-let toggleButton = () => {
+let toggleRecipe = () => {
     if(CALIFORNIA.style.display === `none`) {
         CALIFORNIA.style.display = `block`;
         WHITE.style.display = `none`;
