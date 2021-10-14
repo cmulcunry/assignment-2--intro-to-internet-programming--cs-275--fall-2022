a54168a924f609ab2440dc0b77181a91a48bed1f
woodsweber <woodswebe@hartford.edu>
2021-10-13 23:45:32 -0400

    Began work on converting decimals to fractions using evil bitwise logic

--- js/app.js
+++ js/app.js
@@ -21,9 +21,27 @@ let toggleRecipe = () => {
 
 let updateQuantities = () => {
     let x = QUANTITY.value;
-    CALIFORNIA.children[2].innerHTML = `1. Combine ${1*x} cups of rice with ${1.6*x} cups of water or broth and ${.8*x} Tbsp olive oil.`;
+    CALIFORNIA.children[2].innerHTML = `1. Combine ${1*x} cups of rice with ${decToFrac(1.6*x)} cups of water or broth and ${.8*x} Tbsp olive oil.`;
     CALIFORNIA.children[6].innerHTML = `For softer rice, increase liquid by ${.4*x} cup and cook time by 5 minutes.`;
     WHITE.children[1].innerHTML = `1. Combine ${1*x} cup of rice with ${2*x} cups of water and ${1*x} Tbsp olive oil.`;
 };
 
 //SELECT.addEventListener(`change`, toggleRecipe());
+
+let gcd = (n,d) => {
+    if(d==0) return n;
+    return gcd(d, n%d | 0);
+};
+
+let length = (x) => {
+    return x.toString.length;
+};
+
+let decToFrac = (x) => {
+    let y = x | 0;
+    let denominator = 10**length(x-y);
+    let numerator = (x-y)*denominator;
+    denominator = denominator/gcd(numerator, denominator);
+    numerator = (numerator/gcd(numerator, denominator))/2;
+    return `${y} ${numerator | 0}/${denominator}`;
+};
