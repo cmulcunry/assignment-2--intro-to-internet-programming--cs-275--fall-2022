afe9c1ca6ad11b2d0489cf38c2e71fd15da47e36
woodsweber <woodswebe@hartford.edu>
2021-10-14 14:49:18 -0400

    Delete clean and compress image methods from gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,8 @@
 const { src, dest, series, watch } = require(`gulp`),
     CSSLinter = require(`gulp-stylelint`),
-    del = require(`del`),
     babel = require(`gulp-babel`),
     htmlCompressor = require(`gulp-htmlmin`),
     htmlValidator = require(`gulp-html`),
-    imageCompressor = require(`gulp-image`),
     jsCompressor = require(`gulp-uglify`),
     jsLinter = require(`gulp-eslint`),
     sass = require(`gulp-sass`)(require(`sass`)),
@@ -104,21 +102,6 @@ let transpileJSForProd = () => {
         .pipe(dest(`prod/scripts`));
 };
 
-let compressImages = () => {
-    return src(`dev/img/**/*`)
-        .pipe(imageCompressor({
-            optipng: ['-i 1', '-strip all', '-fix', '-o7', '-force'],
-            pngquant: ['--speed=1', '--force', 256],
-            zopflipng: ['-y', '--lossy_8bit', '--lossy_transparent'],
-            jpegRecompress: ['--strip', '--quality', 'medium', '--min', 40, '--max', 80],
-            mozjpeg: ['-optimize', '-progressive'],
-            gifsicle: ['--optimize'],
-            svgo: ['--enable', 'cleanupIDs', '--disable', 'convertColors'],
-            quiet: false
-        }))
-        .pipe(dest(`prod/img`));
-};
-
 let copyUnprocessedAssetsForProd = () => {
     return src([
         `dev/*.*`,       // Source all files,
@@ -159,26 +142,6 @@ let serve = () => {
     watch(`dev/img/**/*`)
         .on(`change`, reload);
 };
-
-async function clean() {
-    let fs = require(`fs`),
-        i,
-        foldersToDelete = [`./temp`, `prod`];
-
-    for (i = 0; i < foldersToDelete.length; i++) {
-        try {
-            fs.accessSync(foldersToDelete[i], fs.F_OK);
-            process.stdout.write(`\n\tThe ` + foldersToDelete[i] +
-                ` directory was found and will be deleted.\n`);
-            del(foldersToDelete[i]);
-        } catch (e) {
-            process.stdout.write(`\n\tThe ` + foldersToDelete[i] +
-                ` directory does NOT exist or is NOT accessible.\n`);
-        }
-    }
-
-    process.stdout.write(`\n`);
-}
 
 async function listTasks () {
     let exec = require(`child_process`).exec;
@@ -225,9 +188,7 @@ exports.transpileJSForDev = transpileJSForDev;
 exports.compressHTML = compressHTML;
 exports.compileCSSForProd = compileCSSForProd;
 exports.transpileJSForProd = transpileJSForProd;
-exports.compressImages = compressImages;
 exports.copyUnprocessedAssetsForProd = copyUnprocessedAssetsForProd;
-exports.clean = clean;
 exports.default = listTasks;
 exports.lintCSS = lintCSS;
 exports.serve = series(
@@ -241,6 +202,5 @@ exports.build = series(
     compressHTML,
     compileCSSForProd,
     transpileJSForProd,
-    compressImages,
     copyUnprocessedAssetsForProd
 );
