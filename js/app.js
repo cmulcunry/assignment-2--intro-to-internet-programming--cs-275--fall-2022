456fe7b20ac5a20547b0ddce329bef07960b99fe
woodsweber <woodswebe@hartford.edu>
2021-10-13 22:55:21 -0400

    Added code to dynamically change recipe quantities based on user input

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const WHITE = document.getElementById(`white`);
 const CALIFORNIA = document.getElementById(`california`);
 const BUTTON = document.getElementById(`button`);
+const QUANTITY = document.getElementById(`input`);
 
 let toggleRecipe = () => {
     if(CALIFORNIA.style.display === `none`) {
@@ -13,4 +14,11 @@ let toggleRecipe = () => {
         WHITE.style.display = `block`;
         BUTTON.innerHTML = `California Sprouted Rice`;
     }
+};
+
+let updateQuantities = () => {
+    let x = QUANTITY.value;
+    CALIFORNIA.children[2].innerHTML = `1. Combine ${1*x} cups of rice with ${1.6*x} cups of water or broth and ${.8*x} Tbsp olive oil.`;
+    CALIFORNIA.children[6].innerHTML = `For softer rice, increase liquid by ${.4*x} cup and cook time by 5 minutes.`;
+    WHITE.children[1].innerHTML = `1. Combine ${1*x} cup of rice with ${2*x} cups of water and ${1*x} Tbsp olive oil.`;
 };
