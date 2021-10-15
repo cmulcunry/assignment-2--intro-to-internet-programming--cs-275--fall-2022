3a371952d39883230df15a1e70729e6e74347ed7
rrliang <42505429+rrliang@users.noreply.github.com>
2021-10-15 01:04:58 -0400

    Updated app.js file
    -Changed it so the .textContent isn't for the main text and its only for the dynamic quantities

--- js/app.js
+++ js/app.js
@@ -1,37 +1,21 @@
 window.onload = () => {
     let whiteRice = document.getElementById(`whiteRice`);
     let caliRice = document.getElementById(`caliRice`);
-    let recipe = document.getElementById(`recipe`);
-    let cups = document.getElementById(`cups`).value;
+    let whiteRecipe = document.getElementById(`whiteRecipe`);
+    let caliRecipe = document.getElementById(`caliRecipe`);
+    let error = document.getElementById(`error`);
     let currentRice = `white`;
 
-    /*https://stackoverflow.com/questions/11832914/how-to-round-to-at-most-2-
-    decimal-places-if-necessary*/
-    function setWhiteRice(cups) {
-        return `Combine `+ cups +` cup(s) of rice with ` +
-            Math.round((cups * 2)*100)/100 +
-            ` cups of water and 1 Tbsp olive oil.\n` +
-            `Bring to a boil, then reduce heat to the lowest setting.\n` +
-            `Cook for about 18 minutes.`;
+    function setWhiteRice(cup) {
+        document.getElementById(`cup`).textContent = cup;
+        document.getElementById(`whiteWater`).textContent = Math.round((cup * 2)*100)/100;
     }
 
-    function setCaliRice(cups) {
-        return `For slightly al dente rice: Combine ` + cups +
-            ` cup(s) of rice with  ` +
-            Math.round((cups * 1.6)*100)/100 +
-            ` cups of water or broth and 1 Tbsp ` +
-            `olive oil.\n` +
-            `Bring to a boil and stir once to mix.\n` +
-            `Reduce heat to low, cover with a tight-fitting lid and cook for 25 ` +
-            `minutes.\n` +
-            `Remove from heat and let stand for 5 minutes.\n` +
-            `Fluff with a fork and serve.\n` +
-            `For softer rice: Increase liquid by 1/2` +
-            ` cup and cook time by 5 minutes.`;
+    function setCaliRice(cup) {
+        document.getElementById(`cups`).textContent = cup;
+        document.getElementById(`caliWater`).textContent = Math.round((cup * 1.6)*100)/100;
     }
 
-    /*https://stackoverflow.com/questions/175739/built-in-way-in-javascript-to-
-    check-if-a-string-is-a-valid-number?page=1&tab=votes#tab-top*/
     function isNumeric(str) {
         if (typeof str !== `string`) return false;
         return !isNaN(str) &&
@@ -39,19 +23,23 @@ window.onload = () => {
         str > 0;
     }
 
-    document.getElementById(`cups`).addEventListener(`keyup`, listener, false);
+    document.getElementById(`givenCups`).addEventListener(`keyup`, listener, false);
     function listener()  {
-        cups = document.getElementById(`cups`).value;
-        if (!isNumeric(cups)) {
-            recipe.textContent =
-            `Error, please input a number.`;
+        let cup = document.getElementById(`givenCups`).value;
+        error.classList.add(`relativeFixed`);
+        if (!isNumeric(cup)) {
+            error.classList.remove(`relativeFixed`);
+            whiteRecipe.classList.add(`relativeFixed`);
+            caliRecipe.classList.add(`relativeFixed`);
         } else {
             if (currentRice === `white`) {
-                recipe.textContent = recipe.textContent =
-                setWhiteRice(document.getElementById(`cups`).value);
+                caliRecipe.classList.add(`relativeFixed`);
+                whiteRecipe.classList.remove(`relativeFixed`);
+                setWhiteRice(cup);
             } else if (currentRice === `cali`) {
-                recipe.textContent = recipe.textContent =
-                setCaliRice(document.getElementById(`cups`).value);
+                whiteRecipe.classList.add(`relativeFixed`);
+                caliRecipe.classList.remove(`relativeFixed`);
+                setCaliRice(cup);
             }
         }
     }
@@ -76,12 +64,8 @@ window.onload = () => {
         whiteRice.style.background=`grey`;
     };
 
-    /*https://stackoverflow.com/questions/511088/use-javascript-to-place-cursor-
-    at-end-of-text-in-text-input-element?page=2&tab=Oldest*/
-    /*https://stackoverflow.com/questions/45827/how-do-you-automatically-set-the-
-    focus-to-a-textbox-when-a-web-page-loads*/
-    const END = document.getElementById(`cups`).value.length;
-    document.getElementById(`cups`).setSelectionRange(END, END);
-    document.getElementById(`cups`).focus();
+    const END = document.getElementById(`givenCups`).value.length;
+    document.getElementById(`givenCups`).setSelectionRange(END, END);
+    document.getElementById(`givenCups`).focus();
     document.getElementById(`whiteRice`).click();
 };
