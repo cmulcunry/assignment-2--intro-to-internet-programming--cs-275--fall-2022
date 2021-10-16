10d74a030eddc8f8a1b1840d1dd0b747e0acefb3
spencers145 <smartypantsspencer@gmail.com>
2021-10-15 23:46:50 -0400

    Created Mostly Unstyled Page
    I have no idea how I might separate this into different commits.

---
+++ js/app.js
@@ -0,0 +1,93 @@
+function init() {
+};
+
+function disableWhite() {
+    document.getElementById("whiteDiv").classList.add("disabled");
+    document.getElementById("whiteButton").classList.remove("disabled");
+};
+
+function disableCali() {
+    document.getElementById("caliDiv").classList.add("disabled");
+    document.getElementById("caliButton").classList.remove("disabled");
+};
+
+function enableWhite() {
+    document.getElementById("whiteDiv").classList.remove("disabled");
+    document.getElementById("whiteButton").classList.add("disabled");
+};
+
+function enableCali() {
+    document.getElementById("caliDiv").classList.remove("disabled");
+    document.getElementById("caliButton").classList.add("disabled");
+};
+
+function changeSelection(toWhat) {
+    if (toWhat === "white") {
+        enableWhite();
+        disableCali();
+    } else {
+        disableWhite();
+        enableCali();
+    }
+};
+
+function swapWhite() {
+    changeSelection("white");
+};
+
+function swapCali() {
+    changeSelection();
+};
+
+function washNumber(washable) {
+    if (isNaN(washable)) return washable;
+
+    washable = Number(washable);
+    if (Math.floor(washable) < washable) {
+        const WASHABLE_DECIMAL = washable - Math.floor(washable);
+        let numerator = 1;
+
+        while (numerator/16 < WASHABLE_DECIMAL) {
+            numerator += 1;
+        }
+
+        let denominator = 16;
+        while (!(numerator % 2)) {
+            numerator /= 2;
+            denominator /= 2;
+        }
+
+        washable = `${washable > 1 ? Math.floor(washable) : ""} ${numerator}/${denominator}`;
+    }
+
+    return washable;
+};
+
+function updateAmount() {
+    let rice = document.getElementById("amountInput").value;
+
+    if (isNaN(rice) || Number(rice) < 0) {
+        rice = 1;
+    } else {
+        rice = Number(rice);
+    }
+
+    updateWhite(rice);
+    updateCali(rice);
+};
+
+function updateWhite(rice) {
+    const RICE_TEXT = `${washNumber(rice)} ${rice === 1 ? "cup" : "cups"}`;
+    const WATER_TEXT = `${washNumber(rice * 2)} ${rice * 2 === 1 ? "cup" : "cups"}`;
+
+    document.getElementById("whiteRice").innerHTML = RICE_TEXT;
+    document.getElementById("whiteWater").innerHTML = WATER_TEXT;
+};
+
+function updateCali(rice) {
+    const RICE_TEXT = `${washNumber(rice)} ${rice === 1 ? "cup" : "cups"}`;
+    const WATER_TEXT = `${washNumber(rice * 8 / 5)} ${washNumber(rice * 8 / 5) === "1" ? "cup" : "cups"}`;
+
+    document.getElementById("caliRice").innerHTML = RICE_TEXT;
+    document.getElementById("caliWater").innerHTML = WATER_TEXT;
+};
