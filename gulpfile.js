638524330785fc82b8edf0bb925998b02b3a88ba
BorjaErika <78446815+BorjaErika@users.noreply.github.com>
2021-10-15 22:32:35 -0400

    updating gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,12 @@
 const { src, dest, series, watch } = require (`gulp`);
 const gulp = require ('gulp');
-const del = require(`del`);
-const sass = require(`gulp-sass`);
+const del = require(`del`)
 const babel = require(`gulp-babel`);
 const htmlCompressor = require(`gulp-htmlmin`);
 //const htmlValidator = require(`gulp-html`);
 const jsLinter = require(`gulp-eslint`);
 const jsCompressor = require(`gulp-uglify`);
 const imageCompressor = require(`gulp-imagemin`);
-const cache = require(`gulp-cache`);
 const browserSync = require(`browser-sync`);
 const reload = browserSync.reload;
 let browserChoice = `default`;
