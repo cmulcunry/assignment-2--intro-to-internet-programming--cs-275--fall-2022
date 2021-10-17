89abceb93011c4311aaedd18811329f62fb4af75
Scottk123 <skennedy@hartford.edu>
2021-10-16 23:03:31 -0400

    Added serve task

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
-const { src, dest, series } = require(`gulp`);
+const { src, dest, series, watch } = require(`gulp`);
 const htmlValidator = require(`gulp-html`);
 const htmlCompressor = require(`gulp-htmlmin`);
 const jsLinter = require(`gulp-eslint`);
 const sass = require(`gulp-sass`)(require(`sass`));
 const babel = require(`gulp-babel`);
+const browserSync = require(`browser-sync`);
+const reload = browserSync.reload;
+
+let browserChoice = `default`;
 
 let validateHTML = () => {
     return src([`html/*.html`, `html/**/*.html`]).pipe(htmlValidator());
@@ -35,9 +39,43 @@ let transpileJSForDev = () => {
         .pipe(babel())
         .pipe(dest(`temp/scripts`));
 };
+
+let serve = () => {
+    browserSync({
+        notify: true,
+        reloadDelay: 50,
+        browser: browserChoice,
+        server: {
+            baseDir: [
+                `temp`,
+                `dev`,
+                `dev/html`
+            ]
+        }
+    });
+
+    watch(`dev/scripts/*.js`, series(lintJS, transpileJSForDev))
+        .on(`change`, reload);
+
+    watch(`dev/styles/scss/**/*.scss`, compileCSSForDev)
+        .on(`change`, reload);
+
+    watch(`dev/html/**/*.html`, validateHTML)
+        .on(`change`, reload);
+
+    watch(`dev/img/**/*`)
+        .on(`change`, reload);
+};
 exports.validateHTML = validateHTML;
 exports.compressHTML = compressHTML;
 exports.HTMLProcessing = series(validateHTML, compressHTML);
 exports.lintJS = lintJS;
 exports.compileCSSForDev = compileCSSForDev;
 exports.transpileJSForDev = transpileJSForDev;
+exports.serve = series(
+    validateHTML,
+    compileCSSForDev,
+    lintJS,
+    transpileJSForDev,
+    serve
+);
