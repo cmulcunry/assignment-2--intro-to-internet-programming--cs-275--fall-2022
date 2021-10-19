490e451278a43e12ba04911065b1f65334916500
Scottk123 <skennedy@hartford.edu>
2021-10-18 23:10:14 -0400

    Added a second button to change recipes

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
 let riceAmount = document.getElementById(`riceAmount`).value;
-let button = document.getElementById(`changeRecipe`);
+let buttonWhiteRice = document.getElementById(`changeRecipeToWhite`);
+let buttonSproutedRice = document.getElementById(`changeRecipeToSprouted`);
 document.getElementById(`cupsAlDente`).value = riceAmount * (2/1.25);
 document.getElementById(`cupsSoft`).value = riceAmount * (2.5/1.25);
 document.getElementById(`white`).hidden = `hidden`;
 
-button.onclick = function changeRecipe() {
+buttonWhiteRice.onclick = function changeRecipeToWhite() {
     document.getElementById(`sproutedCalifornia`).hidden = `hidden`;
     document.getElementById(`white`).hidden = ``;
+};
+
+buttonSproutedRice.onclick = function changeRecipeToSprouted() {
+    document.getElementById(`sproutedCalifornia`).hidden = ``;
+    document.getElementById(`white`).hidden = `hidden`;
 };
