c2470417a627158551704fbfbf90c8f001b91c2a
Scottk123 <skennedy@hartford.edu>
2021-10-19 22:53:01 -0400

    Cleaned up javascript code

--- js/app.js
+++ js/app.js
@@ -3,31 +3,32 @@ let riceAmount = input.value;
 let buttonWhiteRice = document.getElementById(`changeRecipeToWhite`);
 let buttonSproutedRice = document.getElementById(`changeRecipeToSprouted`);
 let h2 = document.querySelector(`h2`);
+let cupsAlDente = document.getElementById(`cupsAlDente`);
+let cupsSoft = document.getElementById(`cupsSoft`);
 let cupsWhite = document.getElementById(`cupsWhite`);
-document.getElementById(`cupsAlDente`).value = `${riceAmount * (2/1.25)}
-cups of water are needed.`;
-document.getElementById(`cupsSoft`).value = `${riceAmount * (2.5/1.25)}
-cups of water are needed.`;
+let sproutedCalifornia = document.getElementById(`sproutedCalifornia`);
+let white = document.getElementById(`white`);
+
+cupsAlDente.value = `${riceAmount * (2/1.25)} cups of water are needed.`;
+cupsSoft.value = `${riceAmount * (2.5/1.25)} cups of water are needed.`;
 cupsWhite.value = `${riceAmount * 2} cups of water are needed.`;
 document.getElementById(`white`).hidden = `hidden`;
 
 input.onkeyup = function input() {
     riceAmount = document.getElementById(`riceAmount`).value;
-    document.getElementById(`cupsAlDente`).value = `${riceAmount * (2/1.25)}
-    cups of water are needed.`;
-    document.getElementById(`cupsSoft`).value = `${riceAmount * (2.5/1.25)}
-    cups of water are needed.`;
+    cupsAlDente.value = `${riceAmount * (2/1.25)} cups of water are needed.`;
+    cupsSoft.value  = `${riceAmount * (2.5/1.25)} cups of water are needed.`;
     cupsWhite.value = `${riceAmount * 2} cups of water are needed.`;
 };
 
 buttonWhiteRice.onclick = function changeRecipeToWhite() {
     h2.innerHTML = `Making White Rice`;
-    document.getElementById(`sproutedCalifornia`).hidden = `hidden`;
+    sproutedCalifornia.hidden = `hidden`;
     document.getElementById(`white`).hidden = ``;
 };
 
 buttonSproutedRice.onclick = function changeRecipeToSprouted() {
     h2.innerHTML = `Making Sprouted California Rice`;
-    document.getElementById(`sproutedCalifornia`).hidden = ``;
-    document.getElementById(`white`).hidden = `hidden`;
+    sproutedCalifornia.hidden = ``;
+    white.hidden = `hidden`;
 };
