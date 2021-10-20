5d1acf10a4294a1c396a6827c1fdb85c2d14a662
Scottk123 <skennedy@hartford.edu>
2021-10-19 22:17:54 -0400

    Added h2 variable

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ let input = document.getElementById(`riceAmount`);
 let riceAmount = document.getElementById(`riceAmount`).value;
 let buttonWhiteRice = document.getElementById(`changeRecipeToWhite`);
 let buttonSproutedRice = document.getElementById(`changeRecipeToSprouted`);
+let h2 = document.querySelector(`h2`);
 document.getElementById(`cupsAlDente`).value = riceAmount * (2/1.25);
 document.getElementById(`cupsSoft`).value = riceAmount * (2.5/1.25);
 document.getElementById(`white`).hidden = `hidden`;
@@ -13,11 +14,13 @@ input.onkeyup = function input() {
 };
 
 buttonWhiteRice.onclick = function changeRecipeToWhite() {
+    h2.innerHTML = `Making White Rice`;
     document.getElementById(`sproutedCalifornia`).hidden = `hidden`;
     document.getElementById(`white`).hidden = ``;
 };
 
 buttonSproutedRice.onclick = function changeRecipeToSprouted() {
+    h2.innerHTML = `Making Sprouted California Rice`;
     document.getElementById(`sproutedCalifornia`).hidden = ``;
     document.getElementById(`white`).hidden = `hidden`;
 };
